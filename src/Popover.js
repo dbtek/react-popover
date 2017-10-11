7391a8feddb8983704c4bc9550082b9fccd38a08
Diego Michel <diego.ivan.perez.michel@gmail.com>
2017-10-10 20:20:01 -0400

    use simple quotes

--- src/Popover.js
+++ src/Popover.js
@@ -1,49 +1,49 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Overlay from "react-overlays/lib/Overlay";
-import PropTypes from "prop-types";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Overlay from 'react-overlays/lib/Overlay';
+import PropTypes from 'prop-types';
 
 const PopoverStyle = {
-  position: "absolute",
-  padding: "0 5px"
+  position: 'absolute',
+  padding: '0 5px'
 };
 
 const PopoverInnerStyle = {
   width: 200,
-  padding: "10px 14px",
-  color: "#666",
-  backgroundColor: "#fff",
-  boxShadow: "0 0 30px rgba(39, 43, 55, 0.15)"
+  padding: '10px 14px',
+  color: '#666',
+  backgroundColor: '#fff',
+  boxShadow: '0 0 30px rgba(39, 43, 55, 0.15)'
 };
 
 const PopoverArrowStyle = {
-  position: "absolute",
+  position: 'absolute',
   width: 0, height: 0,
-  borderRightColor: "transparent",
-  borderLeftColor: "transparent",
-  borderTopColor: "transparent",
-  borderBottomColor: "transparent",
-  borderStyle: "solid",
+  borderRightColor: 'transparent',
+  borderLeftColor: 'transparent',
+  borderTopColor: 'transparent',
+  borderBottomColor: 'transparent',
+  borderStyle: 'solid',
 };
 
 const PlacementStyles = {
   left: {
-    tooltip: {marginLeft: -3, padding: "0 5px"},
+    tooltip: {marginLeft: -3, padding: '0 5px'},
     arrow: {
-      right: 0, marginTop: -5, borderWidth: "5px 0 5px 5px", borderLeftColor: "#fff"
+      right: 0, marginTop: -5, borderWidth: '5px 0 5px 5px', borderLeftColor: '#fff'
     }
   },
   right: {
-    tooltip: {marginRight: 3, padding: "0 5px"},
-    arrow: {left: 0, marginTop: -5, borderWidth: "5px 5px 5px 0", borderRightColor: "#fff"}
+    tooltip: {marginRight: 3, padding: '0 5px'},
+    arrow: {left: 0, marginTop: -5, borderWidth: '5px 5px 5px 0', borderRightColor: '#fff'}
   },
   top: {
-    tooltip: {marginTop: -3, padding: "5px 0"},
-    arrow: {bottom: 0, marginLeft: -5, borderWidth: "5px 5px 0", borderTopColor: "#fff"}
+    tooltip: {marginTop: -3, padding: '5px 0'},
+    arrow: {bottom: 0, marginLeft: -5, borderWidth: '5px 5px 0', borderTopColor: '#fff'}
   },
   bottom: {
-    tooltip: {marginBottom: 3, padding: "5px 0"},
-    arrow: {top: 0, marginLeft: -5, borderWidth: "0 5px 5px", borderBottomColor: "#fff"}
+    tooltip: {marginBottom: 3, padding: '5px 0'},
+    arrow: {top: 0, marginLeft: -5, borderWidth: '0 5px 5px', borderBottomColor: '#fff'}
   }
 };
 
@@ -98,7 +98,7 @@ Popover.propTypes = {
 Popover.defaultProps = {
   onHide: () => {
   },
-  placement: "bottom",
+  placement: 'bottom',
   hideWithOutsideClick: true
 };
 
