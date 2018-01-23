ef2da55843f7ecf89310240ac1a825a5f9d005b3
İsmail Demirbilek <ce.demirbilek@gmail.com>
2018-01-23 10:26:40 +0300

    added example

---
+++ example/index.js
@@ -0,0 +1,49 @@
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import Popover from '../';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
+
+  handleClick(e) {
+    this.setState({open: !this.state.open});
+  }
+
+  handleClose(e) {
+    this.setState({open: false});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React Popover</h2>
+          <p>
+            <a href="https://github.com/dbtek/react-popover">Github</a>
+          </p>
+        </div>
+        <p className="App-intro" >
+          To see popover <a href="#" ref="target" onClick={this.handleClick.bind(this)}>click here</a>.
+          <Popover
+            placement='bottom'
+            container={this}
+            target={this.refs.target}
+            show={this.state.open}
+            onHide={this.handleClose.bind(this)} >
+            <p>Hey! This is the popover. Click outside to close it.</p>
+          </Popover>
+        </p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
